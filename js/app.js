fc105f66b1cdafb327004058bfa19148262093ee
Alvaro Castillo <alvaro.castillo777@gmail.com>
2026-01-02 16:17:47 -0600

    Refactor README, HTML, CSS, and JavaScript files for improved structure and clarity

--- js/app.js
+++ js/app.js
@@ -1,40 +1,42 @@
-import { API } from './api.js';
-import * as UI from './interfaz.js';
+import { API } from "./api.js";
 
-UI.formularioBuscar.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    //obtener datos del usuario
-    const artista = document.querySelector('#artista').value,
-          cancion = document.querySelector('#cancion').value;
-            // el formulario esta vacio y muestra error
-          if (artista === '' || cancion === '') {
-              UI.divMensajes.innerHTML = 'Error.... todos los datos son obligatorios';
-              UI.divMensajes.classList.add('error');
-              setTimeout(() => {
-                UI.divMensajes.innerHTML = '';
-                UI.divMensajes.classList.remove('error');
-              }, 3000);
-          } else {
-            // el formulario esta lleno y muestra la api
-            const api = new API(artista, cancion);
-            api.consultarAPI()
-            .then(data => {
-                if (data.respuesta.lyrics) {
-                    // la cancion existe
-                    const letra = data.respuesta.lyrics;
-                    UI.divResultado.textContent = letra;
-                } else {
-                    // la cancion no existe
-                    UI.divMensajes.innerHTML = 'Error.... la canción no existe prueba con otra busqueda';
-                    UI.divMensajes.classList.add('error');
-                    setTimeout(() => {
-                      UI.divMensajes.innerHTML = '';
-                      UI.divMensajes.classList.remove('error');
-                      UI.formularioBuscar.reset();
-                    }, 3000);
-                }
-            });
-          }
+export const searchForm = document.querySelector("#search-form"),
+  searchDiv = document.querySelector("#search"),
+  messagesDiv = document.querySelector("#messages"),
+  resultDiv = document.querySelector("#result");
 
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const artist = document.querySelector("#artist").value,
+    song = document.querySelector("#song").value;
+
+  if (artist === "" || song === "") {
+    messagesDiv.innerHTML = "Error.... all fields are required";
+    messagesDiv.classList.add("error");
+    setTimeout(() => {
+      messagesDiv.innerHTML = "";
+      messagesDiv.classList.remove("error");
+    }, 3000);
+  } else {
+    // the form is correct, consult the API
+    const api = new API(artist, song);
+    api.getLyrics().then((data) => {
+      if (data.respuesta.lyrics) {
+        // song already exists
+        const letra = data.respuesta.lyrics;
+        resultDiv.textContent = letra;
+      } else {
+        // song does not exist
+        messagesDiv.innerHTML =
+          "Error.... the song does not exist, try another search";
+        messagesDiv.classList.add("error");
+        setTimeout(() => {
+          messagesDiv.innerHTML = "";
+          messagesDiv.classList.remove("error");
+          searchForm.reset();
+        }, 3000);
+      }
+    });
+  }
 });
